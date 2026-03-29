c18317397b9d22f02ca861fe6a160e617c382686
luckxander <lusenabh@gmail.com>
2026-03-29 15:03:27 -0300

    URL add

---
+++ main.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys 
+
+# Fake url for shopping cart automation
+
+driver = webdriver.Chrome() 
+
+driver.get("https://www.saucedemo.com/")
