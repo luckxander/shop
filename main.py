489d78de0dd81c4be74ae9df2917116e51d0e8e5
luckxander <lusenabh@gmail.com>
2026-03-31 11:18:58 -0300

    Report generating

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import os
-import time
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from datetime import datetime
+from jinja2 import Environment, FileSystemLoader
 
 # Fake url for shopping cart automation
 
 # In the same folder create a file named .env with credentials
+# Loads variables from .env
+load_dotenv()  
 # Retrieve credentials from environment variables
 username_val = os.getenv('MY_APP_USERNAME')
 password_val = os.getenv('MY_APP_PASSWORD')
@@ -31,19 +34,22 @@ chrome_options.add_argument("--disable-save-password-bubble")
 driver = webdriver.Chrome(options=chrome_options) 
 driver.maximize_window()
 
-product_url = "https://www.saucedemo.com/inventory.html"
-cart_url = "https://www.saucedemo.com/cart.html"
-thanks_url = "https://www.saucedemo.com/checkout-complete.html"
-
 # --- login ---
 def login(username_val, password_val):
-    driver.get("https://www.saucedemo.com/")
-    username_field = driver.find_element(By.ID, "user-name") 
-    password_field = driver.find_element(By.ID, "password") 
-    login_button = driver.find_element(By.ID, "login-button") 
-    username_field.send_keys(username_val)
-    password_field.send_keys(password_val)
-    login_button.click()
+    try:
+        driver.get("https://www.saucedemo.com/")
+        username_field = driver.find_element(By.ID, "user-name") 
+        password_field = driver.find_element(By.ID, "password") 
+        login_button = driver.find_element(By.ID, "login-button") 
+        username_field.send_keys(username_val)
+        password_field.send_keys(password_val)
+        login_button.click()
+        print("Login successful!")
+        login = "Login successful!"
+    except Exception as e:
+        print(f"Failed to login: {e}")
+        login = f"Failed to login: {e}"
+    return login
 
 # --- add to cart --- 
 def add_to_cart(product_url):
@@ -57,8 +63,11 @@ def add_to_cart(product_url):
         shopping_cart = add_to_cart_btn = wait.until(EC.element_to_be_clickable(driver.find_element(By.CSS_SELECTOR, "[data-test='shopping-cart-link']")))
         shopping_cart.click()
         print("Item added to cart!")
+        cart = "Item added to cart!"
     except Exception as e:
         print(f"Failed to add item: {e}")
+        cart = f"Failed to add item: {e}"
+    return cart
 
 # -- checkout --
 def checkout(cart_url):
@@ -82,8 +91,11 @@ def checkout(cart_url):
         finish_btn = driver.find_element(By.ID, "finish")  
         finish_btn.click()
         print("Checkout OK")
+        checkout = "Checkout OK"
     except Exception as e:
-        print(f"Failed to checkout: {e}")    
+        print(f"Failed to checkout: {e}")  
+        checkout = f"Failed to checkout: {e}" 
+    return checkout
 
 # --- checkout completed --- 
 def checkout_complete(thanks_url):
@@ -94,16 +106,50 @@ def checkout_complete(thanks_url):
         thanks = element.text
         assert(thank_you == thanks)
         print("Thank you page displayed")
+        confirm = "Thank you page displayed"
         back_home_btn = driver.find_element(By.NAME, "back-to-products")
         back_home_btn.click()
-        print("Back to Home")
+        flag = "Products"
+        element = driver.find_element(By.CSS_SELECTOR, "[data-test='title']")
+        homepage = element.text
+        assert(homepage == flag)
+        print("Returned to Home")
+        home = "Returned to Home"
     except Exception as e:
         print(f"Failed to back home: {e}")
+        confirm = f"Failed to confirm: {e}"
+        home = f"Failed back to home: {e}"
+    return confirm, home
+
+# Shopping Cart URLs after login successful
+product_url = "https://www.saucedemo.com/inventory.html"
+cart_url = "https://www.saucedemo.com/cart.html"
+thanks_url = "https://www.saucedemo.com/checkout-complete.html"
     
 # --- Execution ---
 if __name__ == "__main__":
-    login(username_val, password_val)
-    add_to_cart(product_url)
-    checkout(cart_url)
-    checkout_complete(thanks_url)
-    driver.quit()
+    current_datetime = datetime.now()
+    datetime_string = current_datetime.strftime('%d-%m-%Y %H:%M:%S')
+    login = login(username_val, password_val)
+    cart = add_to_cart(product_url)
+    checkout = checkout(cart_url)
+    values = checkout_complete(thanks_url)
+    confirm, home = values
+    
+    # Set up the Jinja2 environment
+    env = Environment(loader=FileSystemLoader('.')) # Load templates from the current directory
+    template = env.get_template('report_template.html')
+    
+    # Render the template with data
+    output = template.render( 
+        datetime_string=datetime_string, 
+        login=login, cart=cart, 
+        checkout=checkout, 
+        confirm=confirm, 
+        home=home)                          
+    
+    # Save the output to a file
+    with open("result.html", "w") as file:
+        file.write(output) 
+
+driver.quit()
